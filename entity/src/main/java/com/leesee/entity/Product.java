c4fec4e206461e1195fcd12709f33a914412f341
leeseetheloswer <1443442148@qq.com>
2024-04-10 22:10:34 +0800

    项目结构初始

---
+++ entity/src/main/java/com/leesee/entity/Product.java
@@ -0,0 +1,12 @@
+package com.leesee.entity;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private Long id;
+    private String productName;
+    private Double price;
+    private Long stock;
+}
+
